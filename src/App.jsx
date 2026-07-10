de4552c99d68196f1b32ad14d8047c04bd97a527
pennyficash <pennyficash@gmail.com>
2026-07-10 11:18:38 +0800

    feat: assemble full landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
+import Navbar from './components/Navbar.jsx'
 import Hero from './components/Hero.jsx'
 import Features from './components/Features.jsx'
+import DashboardMockup from './components/DashboardMockup.jsx'
+import Security from './components/Security.jsx'
+import Footer from './components/Footer.jsx'
 
 export default function App(){
- return (
-  <>
-   <Hero />
-   <Features />
-  </>
- )
+ return <>
+  <Navbar />
+  <Hero />
+  <Features />
+  <DashboardMockup />
+  <Security />
+  <Footer />
+ </>
 }

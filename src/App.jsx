aebea155599e9d0dedbe36da148074c2e42c558e
pennyficash <pennyficash@gmail.com>
2026-07-10 11:22:36 +0800

    feat: add stats and trust sections

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,10 @@ import Navbar from './components/Navbar.jsx'
 import Hero from './components/Hero.jsx'
 import Features from './components/Features.jsx'
 import DashboardMockup from './components/DashboardMockup.jsx'
+import Stats from './components/Stats.jsx'
 import AIAssistant from './components/AIAssistant.jsx'
 import Security from './components/Security.jsx'
+import Trust from './components/Trust.jsx'
 import Roadmap from './components/Roadmap.jsx'
 import FAQ from './components/FAQ.jsx'
 import Waitlist from './components/Waitlist.jsx'
@@ -15,8 +17,10 @@ export default function App(){
   <Hero />
   <Features />
   <DashboardMockup />
+  <Stats />
   <AIAssistant />
   <Security />
+  <Trust />
   <Roadmap />
   <FAQ />
   <Waitlist />

361e500d7f787cafa1a2623c5f550bcca1932cca
pennyficash <pennyficash@gmail.com>
2026-07-10 11:19:37 +0800

    feat: add waitlist section

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,9 @@ import Navbar from './components/Navbar.jsx'
 import Hero from './components/Hero.jsx'
 import Features from './components/Features.jsx'
 import DashboardMockup from './components/DashboardMockup.jsx'
-import Security from './components/Security.jsx'
 import AIAssistant from './components/AIAssistant.jsx'
+import Security from './components/Security.jsx'
+import Waitlist from './components/Waitlist.jsx'
 import Footer from './components/Footer.jsx'
 
 export default function App(){
@@ -14,6 +15,7 @@ export default function App(){
   <DashboardMockup />
   <AIAssistant />
   <Security />
+  <Waitlist />
   <Footer />
  </>
 }
